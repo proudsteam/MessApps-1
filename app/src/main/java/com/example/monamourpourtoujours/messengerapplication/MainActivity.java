34d8ade3ae73b4e85372d9ee0043f4807208c5bb
baster <legushka9361@yandex.ru>
2019-02-24 19:44:29 +0300

    Test

--- app/src/main/java/com/example/monamourpourtoujours/messengerapplication/MainActivity.java
+++ app/src/main/java/com/example/monamourpourtoujours/messengerapplication/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Jenya;
-        Hello;
+        dsf;
     }
 }
