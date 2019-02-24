0ce28b14bcda7b605eeaf57c2f9297c793208356
baster <legushka9361@yandex.ru>
2019-02-24 19:56:06 +0300

    Test5

--- app/src/main/java/com/example/monamourpourtoujours/messengerapplication/MainActivity.java
+++ app/src/main/java/com/example/monamourpourtoujours/messengerapplication/MainActivity.java
@@ -7,6 +7,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Privet;
+        asjfnds;
+        sdafasd;
+        asdf;
     }
 }
