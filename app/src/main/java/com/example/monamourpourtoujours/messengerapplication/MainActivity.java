51f5307b3536202eb2a211ee00d030de814fad20
utimur <akva_ten@mail.ru>
2019-02-24 18:22:33 +0300

    Тестируем

--- app/src/main/java/com/example/monamourpourtoujours/messengerapplication/MainActivity.java
+++ app/src/main/java/com/example/monamourpourtoujours/messengerapplication/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.monamourpourtoujours.messengerapplication;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 }
